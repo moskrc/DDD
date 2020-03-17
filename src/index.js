6ea4dcc770b53ceedec67e3ee69033c5f5a4e962
lirioblanko <goldliliya@mail.ru>
2020-03-17 15:00:04 +0300

    scroll for main block

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import "./styles/style.css";
 // scripts
 import "./scripts/script.js";
 
+const headerSquarePic = document.querySelector('.header__square-pic');
+
+document.addEventListener('scroll', function () {
+    console.log(window.pageXOffset);
+    headerSquarePic.style.left = -window.pageYOffset+"px";
+})
+
